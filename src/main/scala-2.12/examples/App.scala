051c574478d858aace8f86ff6787c863d1529ce5
Alexandre Kremlianski <kremlianski@gmail.com>
2016-12-25 15:38:21 +0300

    add example 2

--- src/main/scala-2.12/examples/App.scala
+++ src/main/scala-2.12/examples/App.scala
@@ -9,6 +9,21 @@ import org.scalajs.dom
 object App {
   @JSExportTopLevel("main")
   def main(): Unit = {
+
+    //example1
     new Sortable(dom.document.getElementById("items"), null)
+
+
+    //example2
+    new Sortable(dom.document.getElementById("bar"), js.Dictionary("group"->"bar", "animation"->100))
+    new Sortable(dom.document.getElementById("foo"), js.Dictionary("group"->"foo", "animation"->100))
+    new Sortable(dom.document.getElementById("qux"), js.Dictionary(
+      "group" -> js.Dictionary(
+        "name" -> "qux",
+        "put" -> js.Array("foo","bar"),
+      "animation" -> 100
+      )
+    )
+    )
   }
 }
