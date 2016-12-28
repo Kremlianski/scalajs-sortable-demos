cf42b8195130da515a4d890cd79188488c4ab6b9
Kremlianski <kremlianski@gmail.com>
2016-12-29 01:32:16 +0300

    ver 0.2

--- build.sbt
+++ build.sbt
@@ -5,19 +5,20 @@ enablePlugins(WorkbenchPlugin)
 
 name := "scalajs-sortable-demos"
 
-version := "0.1"
+version := "0.2"
 
 scalaVersion := "2.12.1"
 
 
 libraryDependencies ++= Seq(
   "org.scala-js" %%% "scalajs-dom" % "0.9.1",
-  "net.scalapro" %%% "sortable-js-facade" % "0.1.1-SNAPSHOT"
+  "net.scalapro" %%% "sortable-js-facade" % "0.2-SNAPSHOT"
 )
 
 jsDependencies ++= Seq(
   "org.webjars.bower" % "github-com-RubaXa-Sortable" % "1.4.2"
     / "1.4.2/Sortable.js" minified "Sortable.min.js"
 )
+
 resolvers +=
   "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
--- src/main/scala-2.12/examples/App.scala
+++ src/main/scala-2.12/examples/App.scala
@@ -1,9 +1,15 @@
 package examples
-import net.scalapro.sortable.Sortable
+
+import net.scalapro.sortable._
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
 import org.scalajs.dom
+import org.scalajs.dom._
+import org.scalajs.dom.raw.HTMLElement
+
+import js.JSConverters._
+import scala.scalajs.js.{Function1, UndefOr}
 
 object App {
   @JSExportTopLevel("main")
@@ -14,40 +20,58 @@ object App {
 
 
     //example2
-    new Sortable(dom.document.getElementById("bar"), js.Dictionary("group"->"bar", "animation"->100))
-    new Sortable(dom.document.getElementById("foo"), js.Dictionary("group"->"foo", "animation"->100))
-    new Sortable(dom.document.getElementById("qux"), js.Dictionary(
+    new Sortable(dom.document.getElementById("bar"), js.Dictionary("group" -> "bar", "animation" -> 100))
+    new Sortable(dom.document.getElementById("foo"), js.Dictionary(
+      "group" -> "foo",
+      "animation" -> 100
+    ))
+    val s = new Sortable(dom.document.getElementById("qux"), js.Dictionary(
       "group" -> js.Dictionary(
         "name" -> "qux",
-        "put" -> js.Array("foo","bar")),
+        "put" -> js.Array("foo", "bar")),
       "animation" -> 100
-      )
+    )
     )
 
     //example3
     //only with 1.5.0
-    val pull: js.Function2[Sortable, Sortable, js.Any] = {(to:Sortable, from: Sortable) => {
+    val pull: js.Function2[Sortable, Sortable, js.Any] = { (to: Sortable, from: Sortable) => {
 
       from.el.children.length match {
         case x if x > 2 => true
         case _ => "clone"
       }
-    }}
+    }
+    }
 
-    val put: js.Function1[Sortable, js.Any] = {(to:Sortable ) => to.el.children.length < 4}
+    val put: js.Function1[Sortable, js.Any] = { (to: Sortable) => to.el.children.length < 4 }
 
-    new Sortable(dom.document.getElementById("foo1"), js.Dictionary("group"->"foo1", "animation"->100))
-    new Sortable(dom.document.getElementById("bar1"), js.Dictionary(
-      "group" -> js.Dictionary(
-        "name" -> "bar1",
-        "put" -> js.Array("qux1"),
-        "pull" -> pull
+    new Sortable(dom.document.getElementById("foo1"), js.Dictionary("group" -> "foo1", "animation" -> 100))
 
-      ),
+    val bar1Prop = new SortableProps {
+      override val group = js.Dictionary(
 
-        "animation" -> 100
+            "name" -> "bar1",
+            "put" -> js.Array("qux1"),
+            "pull" -> pull
+
+
       )
-    )
+      override val animation = 100
+    }
+
+//    val bar1Prop = js.Dictionary(
+//      "group" -> js.Dictionary(
+//        "name" -> "bar1",
+//        "put" -> js.Array("qux1"),
+//        "pull" -> pull
+//
+//      ),
+//
+//      "animation" -> 100
+//    )
+    new Sortable(dom.document.getElementById("bar1"), bar1Prop)
+
     new Sortable(dom.document.getElementById("qux1"), js.Dictionary(
       "group" -> js.Dictionary(
         "name" -> "qux1",
@@ -79,7 +103,147 @@ object App {
 
       "delay" -> 400
 
-    )
-    )
+    ))
+
+    // example 6
+    // disable
+
+    val sortable = new Sortable(dom.document.getElementById("list1"), null)
+    val switcher = dom.document.getElementById("switcher")
+    switcher.asInstanceOf[HTMLElement].onclick = (event: Event) => {
+      val state = sortable.option("disabled").asInstanceOf[Boolean] // get
+
+      sortable.option("disabled", !state) // set
+
+      switcher.innerHTML = state match {
+        case true => "disable"
+        case false => "enable"
+      }
+    }
+
+    // example 7
+    // handle
+    new Sortable(dom.document.getElementById("listWithHandle"), js.Dictionary(
+      "handle" -> ".my-handle",
+      "animation" -> 150
+    ))
+
+    // example 8
+    // ghost
+    new Sortable(dom.document.getElementById("simpleList"), js.Dictionary(
+      "ghostClass" -> "ghost",
+      "animation" -> 350
+    ))
+
+    // example 9
+    // chosen
+    new Sortable(dom.document.getElementById("chosenList"), js.Dictionary(
+      "ghostClass" -> "ghost1",
+      "delay" -> 50,
+      "chosenClass" -> "chosen",
+      "animation" -> 200
+    ))
+
+    // example 10
+    // force callback
+    new Sortable(dom.document.getElementById("foo2"), js.Dictionary(
+      "group" -> "foo2",
+      "animation" -> 100,
+      "forceFallback" -> true,
+      "fallbackClass" -> "good"
+
+    ))
   }
+
+  //example11
+  //Scroll
+  Sortable(dom.document.getElementById("scrollList"))
+
+
+  //example12
+  //Events
+  val props = js.Dictionary("group" -> "share", "animation" -> 100)
+
+  val e1 = Sortable(dom.document.getElementById("eventsList1"), props)
+  val e2 = Sortable(dom.document.getElementById("eventsList2"), props)
+
+  val fs: js.Function1[EventS, Unit] = (event) => {
+    console info event.`type`
+    console log event
+  }
+  val fm: js.Function1[EventM, Unit] = (event) => {
+    console info event.`type`
+    console log event
+  }
+
+  e1.option("onChoose", fs)
+  e1.option("onStart", fs)
+  e1.option("onEnd", fs)
+  e1.option("onAdd", fs)
+  e1.option("onUpdate", fs)
+  e1.option("onSort", fs)
+  e1.option("onRemove", fs)
+  e1.option("onFilter", fs)
+  e1.option("onClone", fs)
+  e1.option("onMove", fm)
+
+
+  e2.option("onChoose", fs)
+  e2.option("onStart", fs)
+  e2.option("onEnd", fs)
+  e2.option("onAdd", fs)
+  e2.option("onUpdate", fs)
+  e2.option("onSort", fs)
+  e2.option("onRemove", fs)
+  e2.option("onFilter", fs)
+  e2.option("onClone", fs)
+  e2.option("onMove", fm)
+
+
+  //example13
+  //Storage
+
+  val get: js.Function1[Sortable, js.Array[String]] = { (sortable: Sortable) => {
+
+
+
+    val order = window.localStorage.getItem(sortable.option("group").name.asInstanceOf[String])
+    order match {
+      case x:String => x.split("|").toJSArray
+      case _ => js.Array[String]()
+    }
+  }}
+
+  val set: js.Function1[Sortable, Unit] = { (sortable: Sortable) => {
+    val order = sortable.toArray()
+    window.localStorage.setItem("localStorage-example", order.join("|"))
+  }}
+
+//  val storeProps = js.Dictionary(
+//    "group" -> "localStorage-example",
+//    "store" -> js.Dictionary(
+//      "get" -> get,
+//      "set" -> set
+//
+//
+//    )
+//  )
+
+
+  val storeProps = new SortableProps{
+    override val group = "localStorage-example"
+    override val store = js.Dictionary(
+      "get" -> get,
+      "set" -> set
+
+    )
+    override val onStart: UndefOr[Function1[EventS, Unit]] = js.defined{
+      (event:EventS) => console log event
+    }
+  }
+
+
+  Sortable(dom.document.getElementById("storage"), storeProps)
+
+
 }
